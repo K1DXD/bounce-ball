6787dbca87d158588490d1cfd79b452c7500cdd8
K1DXD <k1dxd1998@gmail.com>
2022-03-01 11:54:50 +0700

    edit canvas for fullscreen

--- js/main.js
+++ js/main.js
@@ -1,62 +1,73 @@
+   function Field() {
+       let width = window.innerWidth;
+       let height = window.innerHeight;
+       this.getWidth = function() {
+           return width;
+       }
+       this.getHeight = function() {
+           return height;
+       }
+       this.setWidth = function() {
+           width = window.innerWidth;
+           return canvas.width = width;
+       }
+       this.setHeight = function() {
+           height = window.innerHeight;
+           return canvas.height = height;
+       }
+   }
+
    function Ball() {
-        let canvas = document.getElementById('canvas');
-        let ctx = canvas.getContext('2d');
-        let ballRadius = 10;
-        let x = canvas.width - ballRadius;
-        let y = canvas.height - ballRadius;
-        let baseSpeedX = Math.random();
-        let baseSpeedY = Math.sqrt(1 - Math.pow(baseSpeedX, 2));
-        let speedX = baseSpeedX;
-        let speedY = baseSpeedY;
-        function drawBall() {
-            ctx.beginPath();
-            ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-            ctx.fillStyle = "red";
-            ctx.fill();
-            ctx.closePath();
-        }
+       let canvas = document.getElementById('canvas');
+       let ctx = canvas.getContext('2d');
+       let ballRadius = 10;
+       let x = field.getWidth() - ballRadius;
+       let y = field.getHeight() - ballRadius;
+       let baseSpeedX = Math.random();
+       let baseSpeedY = Math.sqrt(1 - Math.pow(baseSpeedX, 2));
+       let speedX = baseSpeedX;
+       let speedY = baseSpeedY;
 
-        this.draw = function() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            if (x + speedX > canvas.width - ballRadius || x + speedX < ballRadius) {
-                speedX = -speedX;
-            }
-            if (y + speedY > canvas.height - ballRadius || y + speedY < ballRadius) {
-                speedY = -speedY;
-            }
-            drawBall();
-            x += speedX;
-            y += speedY;
-        }
+       function drawBall() {
+           ctx.beginPath();
+           ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+           ctx.fillStyle = "red";
+           ctx.fill();
+           ctx.closePath();
+       }
 
-        this.speedButton = function (speed) {
-            if (speed == 1) {
-                if (speedX > 0) speedX += baseSpeedX;
-                else speedX -= baseSpeedX;
-                if (speedY > 0) speedY += baseSpeedY;
-                else speedY -= baseSpeedY;
-            } else {
-                if (speedX > 0) speedX -= baseSpeedX;
-                else speedX += baseSpeedX;
-                if (speedY > 0) speedY -= baseSpeedY;
-                else speedY += baseSpeedY;
-            }
-        }
+       this.draw = function() {
+           ctx.clearRect(0, 0, canvas.width, canvas.height);
+           if (x + speedX > canvas.width - ballRadius || x + speedX < ballRadius) {
+               speedX = -speedX;
+           }
+           if (y + speedY > canvas.height - ballRadius || y + speedY < ballRadius) {
+               speedY = -speedY;
+           }
+           drawBall();
+           x += speedX;
+           y += speedY;
+       }
 
-        this.speedKeyBroad = function (key) {
-            if (key.key == 'ArrowUp') {
-                if (speedX > 0) speedX += baseSpeedX;
-                else speedX -= baseSpeedX;
-                if (speedY > 0) speedY += baseSpeedY;
-                else speedY -= baseSpeedY;
-            } else if (key.key == 'ArrowDown') {
-                if (speedX > 0) speedX -= baseSpeedX;
-                else speedX += baseSpeedX;
-                if (speedY > 0) speedY -= baseSpeedY;
-                else speedY += baseSpeedY;
-            }
-        }
-    }
-    let ball = new Ball();
-    setInterval(ball.draw, 10);
-    document.addEventListener('keydown', ball.speedKeyBroad);
+       this.speedKeyPress = function(key) {
+           if (key.key == 'ArrowUp') {
+               if (speedX > 0) speedX += baseSpeedX;
+               else speedX -= baseSpeedX;
+               if (speedY > 0) speedY += baseSpeedY;
+               else speedY -= baseSpeedY;
+           } else if (key.key == 'ArrowDown') {
+               if (speedX > 0) speedX -= baseSpeedX;
+               else speedX += baseSpeedX;
+               if (speedY > 0) speedY -= baseSpeedY;
+               else speedY += baseSpeedY;
+           }
+       }
+   }
+   let field = new Field();
+   let ball = new Ball();
+   setInterval(function() {
+       field.setWidth();
+       field.setHeight();
+       ball.draw();
+   }, 10);
+   document.addEventListener('keydown', ball.speedKeyPress);
